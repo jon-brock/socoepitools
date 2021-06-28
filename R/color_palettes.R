edb2cadf0d4ab06b6ccac8d9c91d232fc81f5dcd
Jon Brock <jpb2210@cumc.columbia.edu>
2021-06-28 14:34:44 -0400

    added a color palette using soco slides.

--- R/color_palettes.R
+++ R/color_palettes.R
@@ -2,5 +2,5 @@
 palettes <-
     list(
         "palette_01" = c("264653", "2A9D8F", "E9C46A", "F4A261", "E76F51"),
-        "palette_02" = c("F94144", "F3722C", "F8961E", "F9C74F", "90BE6D", "43AA8B", "577590")
-    )
+        "palette_02" = c("F94144", "F3722C", "F8961E", "F9C74F", "90BE6D", "43AA8B", "577590"),
+        "soco_official" = c("#196E62", "#B7BC85", "#D59F10", "#AB0C36"))
